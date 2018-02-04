0667569848b8c183f818ad529e802f9091ef1741
Anna Watson <annawatsonfly@gmail.com>
2018-02-05 10:22:10 +1100

    navbar

--- htmlUPDATE/ViewController.swift
+++ htmlUPDATE/ViewController.swift
@@ -14,10 +14,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+
+        navigationController?.setNavigationBarHidden(false, animated: true)
+        
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        //chnage
     }
 
 
