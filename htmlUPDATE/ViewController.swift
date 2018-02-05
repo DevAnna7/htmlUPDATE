bc672efa7ffdb7c30eff95eafe1a3c86b9684455
DevAnna7 <annawatsonfly@gmail.com>
2018-02-05 18:08:36 +1100

    navbar

--- htmlUPDATE/ViewController.swift
+++ htmlUPDATE/ViewController.swift
@@ -32,7 +32,7 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-        //chnage
+        // Trying to push to repo
     }
 
 
